cff9779fe5128a55d1dfc12e3d9fa81c03736e62
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2021-03-16 18:16:57 +0600

    Online Judge Solutions

---
+++ HackerRank/ACM_ICPC.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+template <typename... T>
+void scan(T &...args) {
+    ((cin >> args), ...);
+}
+
+template <typename... T>
+void print(T... args) {
+    ((cout << args), ...);
+}
+
+class Solution_To_Problem {
+    // variables
+    int a, b, c, d, m, n, p, q, s, t, x, y, z;
+    int test_case, count, count1, count2, sum, sum1, sum2, temp;
+    ll r, l, k;
+    double e, f, g, h;
+    char ch, ch1, ch2;
+    bool flag, check;
+    string str, s1, s2;
+    // constants
+    const ld Pi = acos(-1);
+    const int Mod = 10'000'007;
+
+   public:
+    void solution_function() {
+#ifdef LOCAL_DEBUG_OUT
+        print("\nOUTPUT:\n");
+#endif
+
+        scan(n, m);
+
+        for (int i = 0; i)
+    }
+} Solution;
+
+int main() {
+#ifdef LOCAL_DEBUG_IN
+    print("INPUT:\n");
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    Solution.solution_function();
+
+    return 0;
+}
