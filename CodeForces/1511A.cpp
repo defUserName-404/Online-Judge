59508055bf656d131c5dea81fed5c0480841bd96
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2021-04-23 18:47:54 +0600

    Online Judge Solutions

---
+++ CodeForces/1511A.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename... T>
+void scan(T &...args) {
+    ((cin >> args), ...);
+}
+
+template <typename... T>
+void print(T... args) {
+    ((cout << args), ...);
+}
+
+class Solution_To_Problem {
+    // variables
+    int n, x;
+    int test_case, count;
+
+   public:
+    void solution_function() {
+#ifdef LOCAL_DEBUG_OUT
+        print("\nOUTPUT:\n");
+#endif
+
+        scan(test_case);
+
+        while (test_case--) {
+            scan(n);
+
+            count = 0;
+
+            for (int i = 0; i < n; i++) {
+                scan(x);
+
+                if (x == 2)
+                    count++;
+            }
+
+            print(n - count, '\n');
+        }
+    }
+} Solution;
+
+int main() {
+#ifdef LOCAL_DEBUG_IN
+    print("INPUT:\n");
+#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    Solution.solution_function();
+
+    return 0;
+}
