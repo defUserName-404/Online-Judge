823991fb0a810dcfe16e76e39ada6a1d47fd9677
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2021-05-07 15:58:23 +0600

    Online Judge Solutions

---
+++ LeetCode/26_RemoveDuplicates.hpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+   public:
+    int removeDuplicates(std::vector<int> &nums) {
+        nums.erase(std::unique(nums.begin(), nums.end()), nums.end());
+
+        return nums.size();
+    }
+};
