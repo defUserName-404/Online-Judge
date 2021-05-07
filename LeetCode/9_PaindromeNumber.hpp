536fa5caaa8aef2cfc1b2b4d0be185f8a2e8fb79
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2021-05-07 13:49:06 +0600

    Online Judge Solutions

---
+++ LeetCode/9_PaindromeNumber.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+
+class Solution {
+   public:
+    bool isPalindrome(int x) {
+        std::string str = std::to_string(x);
+        std::string str_rev = str;
+        std::reverse(str_rev.begin(), str_rev.end());
+
+        return (str == str_rev);
+    }
+};
