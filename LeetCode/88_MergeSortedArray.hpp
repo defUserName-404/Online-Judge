d55c508475176df12d69b8f208c8b0bae1e4e16d
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2021-05-08 06:42:24 +0600

    Online Judge Solutions

---
+++ LeetCode/88_MergeSortedArray.hpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+class Solution {
+   public:
+    void merge(std::vector<int> &nums1, int m, std::vector<int> &nums2, int n) {
+        // removing 0's
+        nums1.erase(nums1.end() - n, nums1.end());
+
+        for (int x : nums2)
+            nums1.push_back(x);
+
+        std::sort(nums1.begin(), nums1.end());
+    }
+};
