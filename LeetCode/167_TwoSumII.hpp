ac492ab3611452fce183f44a5f589957f8b92f19
defUserName-404 <moshiur.r.nafi@gmail.com>
2021-05-15 17:05:59 +0600

    Online Judge Solutions

--- LeetCode/167_TwoSumII.hpp
+++ LeetCode/167_TwoSumII.hpp
@@ -1,10 +1,26 @@
 #include <vector>
+#include <unordered_map>
 
 class Solution
 {
   public:
     std::vector<int> twoSum(std::vector<int> &numbers, int target)
     {
+        std::unordered_map<int, int> indices;
+        std::vector<int> ans;
 
+        int n = numbers.size();
+
+        for (int i = 0; i < n; i++)
+        {
+            if (indices.find(target - numbers[i]) != indices.end())
+            {
+                ans.push_back(indices[target - numbers[i]] + 1);
+                ans.push_back(i + 1);
+            }
+            indices[numbers[i]] = i;
+        }
+
+        return ans;
     }
 };
