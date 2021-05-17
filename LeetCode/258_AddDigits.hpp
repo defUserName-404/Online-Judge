ad3c564cfdc8c10cf0c7e62e561220799f97791e
defUserName-404 <moshiur.r.nafi@gmail.com>
2021-05-17 13:57:36 +0600

    Online Judge Solutions

---
+++ LeetCode/258_AddDigits.hpp
@@ -0,0 +1,19 @@
+class Solution
+{
+  public:
+    int addDigits(int num)
+    {
+        int temp = 0;
+
+        while (num > 0)
+        {
+            temp += (num % 10);
+            num /= 10;
+        }
+
+        if (temp >= 10)
+             return addDigits(temp);
+
+        return temp;
+    }
+};
