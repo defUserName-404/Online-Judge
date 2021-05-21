2d18558b5cc2fd35d87cb9097a590c76c9837746
defUserName-404 <moshiur.r.nafi@gmail.com>
2021-05-21 21:37:33 +0600

    Online Judge Solutions

--- LeetCode/657_RobotReturnsToOrigin.py
+++ LeetCode/657_RobotReturnsToOrigin.py
@@ -1,15 +1,6 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        x, y = 0, 0
+        x = (moves.count('L') == moves.count('R'))
+        y = (moves.count('U') == moves.count('D'))
 
-        for ch in moves:
-            if ch == 'L':
-                x = x - 1
-            elif ch == 'D':
-                y = y - 1
-            elif ch == 'R':
-                x = x + 1
-            else:
-                y = y + 1
-
-        return x == 0 and y == 0 
+        return x and y
