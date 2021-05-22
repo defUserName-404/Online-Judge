7acb4741a583e962e005f43c2e4e887654e225a2
defUserName-404 <moshiur.r.nafi@gmail.com>
2021-05-22 20:30:23 +0600

    Online Judge Solutions

---
+++ LeetCode/905_SortArrayByParity.py
@@ -0,0 +1,13 @@
+from typing import List, NoReturn
+
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        new_list = []
+
+        for i in nums:
+            if i % 2 == 0:
+                new_list.insert(0, i)
+            else:
+                new_list.append(i)
+        
+        return new_list
