1a07f262f7ac57222ba23f8666cbd364df1323d6
defUserName-404 <moshiur.r.nafi@gmail.com>
2021-05-28 22:57:26 +0600

    Online Judge Solutions

--- LeetCode/509_FibonacciNumber.py
+++ LeetCode/509_FibonacciNumber.py
@@ -1,8 +1,8 @@
 class Solution:
     def fib(self, n: int) -> int:
-        fibonacci = [0, 0, 1]
+        fibonacci = [0, 1]
 
         for i in range(2, n + 1):
-            fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2]
+            fibonacci.append(fibonacci[i - 1] + fibonacci[i - 2])
 
         return fibonacci[n]
