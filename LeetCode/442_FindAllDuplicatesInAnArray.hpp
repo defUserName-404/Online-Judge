7da815fd251739c44baa202d73dc078adece6ac1
defUserName-404 <moshiur.r.nafi@gmail.com>
2021-06-01 15:13:39 +0600

    Online Judge Solutions

---
+++ LeetCode/442_FindAllDuplicatesInAnArray.hpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <set>
+
+class Solution
+{
+  public:
+    std::vector<int> findDuplicates(std::vector<int> &nums);
+};
+
+std::vector<int> Solution::findDuplicates(std::vector<int> &nums)
+{
+    std::set<int> Count;
+    std::vector<int> result;
+
+    for (int &x : nums)
+    {
+        if (Count.count(x))
+            result.push_back(x);
+        else
+            Count.insert(x);
+    }
+
+    return result;
+}
