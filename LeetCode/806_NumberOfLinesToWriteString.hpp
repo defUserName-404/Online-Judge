daef9731c23a52f1a4ef3e5246d53d6860bb6419
defUserName-404 <moshiur.r.nafi@gmail.com>
2021-06-07 22:26:12 +0600

    Online Judge Solutions

---
+++ LeetCode/806_NumberOfLinesToWriteString.hpp
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+  public:
+    std::vector<int> numberOfLines(std::vector<int> &widths, std::string s);
+};
+
+std::vector<int> Solution::numberOfLines(std::vector<int> &widths, std::string s)
+{
+    int currentWidth = 0, lines = 1;
+
+    for (const char &ch : s)
+    {
+        int value = widths[ch - 'a'];
+
+        if (currentWidth + value <= 100)
+            currentWidth += value;
+
+        else
+            lines++, currentWidth = value;
+    }
+
+    return {lines, currentWidth};
+}
