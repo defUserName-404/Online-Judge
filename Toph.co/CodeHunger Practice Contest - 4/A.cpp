e2d5146178684c3e55ab465cfe66888da713feef
defUserName-404 <moshiur.r.nafi@gmail.com>
2021-06-19 21:08:15 +0600

    Online Judge Solutions

---
+++ Toph.co/CodeHunger Practice Contest - 4/A.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename... T>
+void scan(T &...args)
+{
+    ((cin >> args), ...);
+}
+
+template <typename... T>
+void print(T... args)
+{
+    ((cout << args), ...);
+}
+
+class Solution
+{
+    // variables
+    int count;
+    string str;
+
+  public:
+    Solution()
+    {
+#ifdef LOCAL_DEBUG_IN
+        print("\033[1;31mINPUT:\n");
+        print("\033[34m");
+#endif
+    }
+
+    void test()
+    {
+#ifdef LOCAL_DEBUG_OUT
+        print("\n\033[1;31mOUTPUT:\n");
+        print("\033[32m");
+#endif
+
+        scan(str);
+
+        count = 0;
+
+        string vowels = "aeiouAEIOU";
+
+        for (const char &ch : str)
+            if (vowels.find(ch) != string::npos)
+                count++;
+
+        print(count, '\n');
+    }
+};
+
+int main()
+{
+    Solution solution;
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solution.test();
+
+    return 0;
+}
