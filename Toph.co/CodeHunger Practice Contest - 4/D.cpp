670af4e197cc54ea01cab7f501c5974f45cd2729
defUserName-404 <moshiur.r.nafi@gmail.com>
2021-06-19 21:28:55 +0600

    Online Judge Solutions

---
+++ Toph.co/CodeHunger Practice Contest - 4/D.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+template <typename... T>
+void scan(T &...args)
+{
+    ((cin >> args), ...);
+}
+
+template <typename... T>
+void print(T... args)
+{
+    ((cout << args), ...);
+}
+
+class Solution
+{
+    // variables
+    int n, l, r, x;
+    int sum;
+
+  public:
+    Solution()
+    {
+#ifdef LOCAL_DEBUG_IN
+        print("\033[1;31mINPUT:\n");
+        print("\033[34m");
+#endif
+    }
+
+    void test()
+    {
+#ifdef LOCAL_DEBUG_OUT
+        print("\n\033[1;31mOUTPUT:\n");
+        print("\033[32m");
+#endif
+
+        scan(n, l, r);
+
+        sum = 0;
+
+        vector<int> arr(n);
+
+        for (auto &x : arr)
+            scan(x);
+
+        for (int i = l; i <= r; i++)
+            sum += arr[i];
+
+        print(sum, '\n');
+    }
+};
+
+int main()
+{
+    Solution solution;
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solution.test();
+
+    return 0;
+}
