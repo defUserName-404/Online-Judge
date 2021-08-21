aebe31558f78818b01cb74fdc3b2823fac17d558
defUserName-404 <moshiur.r.nafi@gmail.com>
2021-08-22 00:30:02 +0600

    Online Judge Solutions

--- LeetCode/67_AddBinary.hpp
+++ LeetCode/67_AddBinary.hpp
@@ -16,22 +16,27 @@ std::string Solution::addBinary(std::string a, std::string b)
     std::string ans;
     int m = a.size(), n = b.size();
     bool carry = false;
-    bool terminatingCondition = std::min(m, n);
     int i = 1;
 
-    while (true)
+    while (i <= std::max(m, n))
     {
-        int x = binarySum(a[m - i], b[n - i], carry);
+        int x;
 
-        if (x == 2)
-            x = 1, carry = true;
+        if (i > m)
+            x = binarySum('0', b[n - i], carry);
+        else if (i > n)
+            x = binarySum(a[m - i], '0', carry);
+        else
+            x = binarySum(a[m - i], b[n - i], carry);
+
+        if (x > 1)
+            x = x - 2, carry = true;
+        else
+            carry = false;
 
         ans += (x + '0');
 
         i++;
-
-        if (i == terminatingCondition)
-            break;
     }
 
     if (carry)
