54468a9806a7691d7cbc1150e0be1de08780aa1e
defUserName-404 <moshiur.r.nafi@gmail.com>
2021-09-09 05:13:02 +0600

    Online Judge Solutions

--- CodeChef/run.sh
+++ CodeChef/run.sh
@@ -4,16 +4,20 @@ fileName=$1
 
 # Color Codes
 BLUE='\033[1;34m'
+YELLOW='\033[1;33m'
 
 # Adding these lines to add to the cpp file
 sed -i '2i#define LOCAL_DEBUG_IN' $fileName.cpp
 sed -i '3i#define LOCAL_DEBUG_OUT' $fileName.cpp
 
 # Compiling
+echo -e "${YELLOW}Compiling..."
 g++ -std=c++17 $fileName.cpp -o $fileName.out
+echo -e "${YELLOW}Done!"
 
 echo -e "${BLUE}"
 
+# Running
 time ./$fileName.out
 
 # Clearing the executable file
--- LightOJ/1012.cpp
+++ LightOJ/1012.cpp
@@ -26,6 +26,8 @@ class Solution_To_Problem {
         scan(test_case);
         cin.ignore();
 
+        isal
+
         for (int tc = 0; tc < test_case; tc++) {
             scan(w, h);
 
