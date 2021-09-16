c944ddfc4e2aae0b81f3a34018a932526c274fb0
defUserName-404 <moshiur.r.nafi@gmail.com>
2021-09-16 22:05:37 +0600

    Online Judge Solutions

--- LeetCode/189_RotateArray.hpp
+++ LeetCode/189_RotateArray.hpp
@@ -3,23 +3,25 @@
 class Solution
 {
   public:
-    void rotate(std::vector<int> &nums, int k)
-    {
-        int n = nums.size();
-
-        std::vector<int> finalPosition(n);
-
-        k %= n;
-
-        for (int i = 0; i < n; i++)
-        {
-            int position = (i + k);
-            position %= n;
-
-            finalPosition[position] = nums[i];
-        }
-
-        for (int i = 0; i < n; i++)
-            nums[i] = finalPosition[i];
-    }
+    void rotate(std::vector<int> &nums, int k);
 };
+
+void Solution::rotate(std::vector<int> &nums, int k)
+{
+    int n = nums.size();
+
+    std::vector<int> finalPosition(n);
+
+    k %= n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int position = (i + k);
+        position %= n;
+
+        finalPosition[position] = nums[i];
+    }
+
+    for (int i = 0; i < n; i++)
+        nums[i] = finalPosition[i];
+}
