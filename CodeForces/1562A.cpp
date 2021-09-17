a0694111f4d687edc24fd85b64fc683b8eeca2fb
defUserName-404 <moshiur.r.nafi@gmail.com>
2021-09-17 08:49:18 +0600

    Online Judge Solutions

---
+++ CodeForces/1562A.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename... T>
+void scan(T &...args)
+{
+    ((cin >> args), ...);
+}
+
+template <typename... T>
+void print(T... args)
+{
+    ((cout << args), ...);
+}
+
+class Solution
+{
+    // variables
+    int test_case, l, r;
+
+  public:
+    Solution()
+    {
+#ifdef LOCAL_DEBUG_IN
+        print("\033[1;31mINPUT:\n");
+        print("\033[34m");
+#endif
+    }
+
+    void test()
+    {
+#ifdef LOCAL_DEBUG_OUT
+        print("\n\033[1;31mOUTPUT:\n");
+        print("\033[32m");
+#endif
+
+        scan(test_case);
+
+        while (test_case--)
+        {
+            scan(l, r);
+
+            if (r < l * 2)
+                print(r - l, '\n');
+            else
+                print((r - 1) / 2, '\n');
+        }
+    }
+};
+
+int main()
+{
+    Solution solution;
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solution.test();
+
+    return 0;
+}
