1ec29285d444fed86dcb92aeee46e551eb1cf588
defUserName-404 <moshiur.r.nafi@gmail.com>
2021-09-17 08:51:47 +0600

    Online Judge Solutions

--- LeetCode/344_ReverseString.hpp
+++ LeetCode/344_ReverseString.hpp
@@ -3,11 +3,14 @@
 class Solution
 {
   public:
-    void reverseString(std::vector<char> &s)
-    {
-        int n = s.size();
-
-        for (int i = 0, j = n - 1; i < n / 2; i++, j--)
-            std::swap(s[i], s[j]);
-    }
+    void reverseString(std::vector<char> &s);
 };
+
+void Solution::reverseString(std::vector<char> &s)
+{
+    int n = s.size();
+
+    for (int i = 0, j = n - 1; i < n / 2; i++, j--)
+
+        std::swap(s[i], s[j]);
+}
