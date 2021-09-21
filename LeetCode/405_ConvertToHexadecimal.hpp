d8d83964a762a5f29675a6991b227406158eb376
defUserName-404 <moshiur.r.nafi@gmail.com>
2021-09-21 15:51:25 +0600

    Online Judge Solutions

---
+++ LeetCode/405_ConvertToHexadecimal.hpp
@@ -0,0 +1,29 @@
+#include <cmath>
+#include <iostream>
+
+class Solution
+{
+  public:
+    std::string toHex(unsigned int num);
+};
+
+std::string Solution::toHex(unsigned int num)
+{
+    std::string hexadecimal;
+
+    if (num == 0)
+        return "0";
+
+    while (num > 0)
+    {
+        int x = num % 16;
+        //* converting the integer to character
+        x += (x >= 10 ? ('a' - 10) : '0');
+        num /= 16;
+        hexadecimal += x;
+    }
+
+    std::reverse(hexadecimal.begin(), hexadecimal.end());
+
+    return hexadecimal;
+}
