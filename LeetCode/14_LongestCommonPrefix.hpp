0e388453b7738448eca5bd2b8d2a24505e4aa15c
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2021-11-21 11:23:24 +0600

    Online Judge Solution

--- LeetCode/14_LongestCommonPrefix.hpp
+++ LeetCode/14_LongestCommonPrefix.hpp
@@ -1,31 +1,33 @@
 #include <iostream>
 #include <vector>
 
-class Solution
-{
-  public:
-    std::string longestCommonPrefix(std::vector<std::string> &strs);
-};
+//! WA
 
-std::string Solution::longestCommonPrefix(std::vector<std::string> &strs)
-{
-    std::string longest;
-    bool isSame = true;
-    int totalStrings = strs.size();
+// class Solution
+// {
+//   public:
+//     std::string longestCommonPrefix(std::vector<std::string> &strs);
+// };
 
-    for (int j = 0;; j++)
-    {
-        for (int i = 0; i < totalStrings - 1; i++)
-        {
-            if (j == strs[i].size() || !isSame)
-                return longest;
+// std::string Solution::longestCommonPrefix(std::vector<std::string> &strs)
+// {
+//     std::string longest;
+//     bool isSame = true;
+//     int totalStrings = strs.size();
 
-            isSame = (strs[i][j] == strs[i + 1][j]);
-        }
+//     for (int j = 0;; j++)
+//     {
+//         for (int i = 0; i < totalStrings - 1; i++)
+//         {
+//             if (j == strs[i].size() || !isSame)
+//                 return longest;
 
-        if (isSame)
-            longest += strs[0][j];
-    }
+//             isSame = (strs[i][j] == strs[i + 1][j]);
+//         }
 
-    return longest;
-}
+//         if (isSame)
+//             longest += strs[0][j];
+//     }
+
+//     return longest;
+// }
