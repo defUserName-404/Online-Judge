fa4106100f0f4880e921c78e921402a25f5bf802
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-01-02 18:41:47 +0600

    Online Judge Solution

---
+++ LeetCode/2124_CheckIfAllAsAppearBeforeBs.hpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <algorithm>
+
+class Solution
+{
+  public:
+    bool checkString(std::string &s);
+};
+
+/*
+?   Solution 1: Accepted
+?   8ms(23.53% faster)    
+*/
+// bool Solution::checkString(std::string s)
+// {
+//     std::string sSorted = s;
+//     std::sort(sSorted.begin(), sSorted.end());
+
+//     return s == sSorted;
+// }
+
+bool Solution::checkString(std::string &s)
+{
+    bool bSeen = false;
+
+    for (const auto &ch : s)
+    {
+        if (!bSeen && ch == 'b')
+            bSeen = true;
+        if (bSeen && ch == 'a')
+            return false;
+    }
+
+    return true;
+}
