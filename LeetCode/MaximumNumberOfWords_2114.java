197ac5bf3e2149474b191eeff106b12cd723ec8c
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-01-06 22:02:41 +0600

    Online Judge Solution

---
+++ LeetCode/MaximumNumberOfWords_2114.java
@@ -0,0 +1,16 @@
+class MaximumNumberOfWords_2114 {
+    public int mostWordsFound(String[] sentences) {
+        int globalMax = 0, current = 0;
+
+        for (String sentence : sentences) {
+            String[] words = sentence.split(" ");
+
+            current = words.length;
+
+            if (current > globalMax)
+                globalMax = current;
+        }
+
+        return globalMax;
+    }
+}
