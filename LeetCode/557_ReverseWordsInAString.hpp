3443ea13675db72b9717d00ac2cd98f3575dfcff
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-01-18 06:40:00 +0600

    Online Judge Solution

--- LeetCode/557_ReverseWordsInAString.hpp
+++ LeetCode/557_ReverseWordsInAString.hpp
@@ -2,30 +2,42 @@
 
 class Solution
 {
+    void reverseStringInPosition(int i, int &left, int &right, bool printSpace,
+                                 std::string &reverse, const std::string &s);
+
   public:
     std::string reverseWords(std::string s);
 };
 
+void Solution::reverseStringInPosition(int i, int &left, int &right, bool printSpace,
+                                       std::string &reverse, const std::string &s)
+{
+    for (int j = left + right; j >= left; j--)
+        reverse += s[j];
+
+    if (printSpace)
+        reverse += ' ';
+
+    left = i + 1;
+    right = -1;
+}
+
 std::string Solution::reverseWords(std::string s)
 {
     int len = s.size();
-    int l = 0, r = 0;
+    int left = 0, right = -1;
     std::string reverse;
 
     for (int i = 0; i < len; i++)
     {
         if (s[i] != ' ')
-            r++;
+            right++;
 
         else
-        {
-            for (int j = r - 1; j >= l; j--)
-                reverse += s[j];
-
-            l = i;
-            reverse += ' ';
-        }
+            reverseStringInPosition(i, left, right, true, reverse, s);
     }
+
+    reverseStringInPosition(left, left, right, false, reverse, s);
 
     return reverse;
 }
--- LeetCode/AlgorithmStudyPlan/557_ReverseWordsInAString.hpp
+++ LeetCode/AlgorithmStudyPlan/557_ReverseWordsInAString.hpp
@@ -2,14 +2,30 @@
 
 class Solution
 {
+    void reverseStringInPosition(int i, int &left, int &right, bool printSpace,
+                                 std::string &reverse, const std::string &s);
+
   public:
     std::string reverseWords(std::string s);
 };
 
+void Solution::reverseStringInPosition(int i, int &left, int &right, bool printSpace,
+                                       std::string &reverse, const std::string &s)
+{
+    for (int j = left + right; j >= left; j--)
+        reverse += s[j];
+
+    if (printSpace)
+        reverse += ' ';
+
+    left = i + 1;
+    right = -1;
+}
+
 std::string Solution::reverseWords(std::string s)
 {
     int len = s.size();
-    int left = 0, right = 0;
+    int left = 0, right = -1;
     std::string reverse;
 
     for (int i = 0; i < len; i++)
@@ -18,14 +34,10 @@ std::string Solution::reverseWords(std::string s)
             right++;
 
         else
-        {
-            for (int j = right - 1; j >= left; j--)
-                reverse += s[j];
-
-            left = i;
-            reverse += ' ';
-        }
+            reverseStringInPosition(i, left, right, true, reverse, s);
     }
+
+    reverseStringInPosition(left, left, right, false, reverse, s);
 
     return reverse;
 }
