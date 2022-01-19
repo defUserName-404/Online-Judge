c54c111f6e87968b76d77af5adcc277f4543fcb2
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-01-19 10:49:27 +0600

    Online Judge Solution

---
+++ LeetCode/AlgorithmStudyPlan/857_MinimumCostToHireKWorkers.hpp
@@ -0,0 +1,23 @@
+#include <map>
+#include <vector>
+
+// TODO: Unsolved
+
+// class Solution
+// {
+//   public:
+//     double mincostToHireWorkers(std::vector<int> &quality, std::vector<int> &wage, int k);
+// };
+
+// double Solution::mincostToHireWorkers(std::vector<int> &quality, std::vector<int> &wage, int k)
+// {
+//     std::multimap<int, int, std::greater<>> mMap;
+
+//     for (int i = 0; i < quality.size(); i++)
+//         mMap.insert(std::make_pair(quality[i], wage[i]));
+
+//     for (const auto it : mMap)
+//     {
+
+//     }
+// }
