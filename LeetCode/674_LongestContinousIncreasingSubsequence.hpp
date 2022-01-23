da8f8421c630a71dc41078a19a74a76de9e7a77e
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-01-23 09:03:08 +0600

    Online Judge Solution

---
+++ LeetCode/674_LongestContinousIncreasingSubsequence.hpp
@@ -0,0 +1,22 @@
+#include <vector>
+
+class Solution
+{
+  public:
+    int findLengthOfLCIS(std::vector<int> &nums);
+};
+
+int Solution::findLengthOfLCIS(std::vector<int> &nums)
+{
+    int n = nums.size();
+    int starting = 0, longest = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (i > 0 && nums[i - 1] >= nums[i])
+            starting = i;
+		longest = std::max(longest, i - starting + 1);
+    }
+
+    return longest;
+}
