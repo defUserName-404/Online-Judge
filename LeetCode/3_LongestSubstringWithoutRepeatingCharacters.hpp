b5224ee8ae24a22703f92b50579ca53556f57267
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-01-23 09:57:08 +0600

    Online Judge Solution

---
+++ LeetCode/3_LongestSubstringWithoutRepeatingCharacters.hpp
@@ -0,0 +1,23 @@
+#include <string>
+
+class Solution
+{
+  public:
+    int lengthOfLongestSubstring(std::string s);
+};
+
+int Solution::lengthOfLongestSubstring(std::string s)
+{
+    int n = s.size();
+    int startingPointOfChars[256] = {[0 ... 255] = -1};
+    int longest = 0, startingPosition = -1;
+
+    for (int i = 0; i < n; i++)
+    {
+        startingPosition = std::max(startingPointOfChars[s[i]], startingPosition);
+        startingPointOfChars[s[i]] = i;
+        longest = std::max(longest, i - startingPosition);
+    }
+
+    return longest;
+}
