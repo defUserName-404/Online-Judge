927f48d61b7241bfb8ccd14842c42a7fafe59ae6
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-01-27 21:47:33 +0600

    Online Judge Solution

--- LeetCode/680-ValidPalindromeII.hpp
+++ LeetCode/680-ValidPalindromeII.hpp
@@ -1,23 +1,40 @@
 #include <iostream>
-#include <map>
 
 class Solution
 {
+    bool isPalindrome(int left, int right, std::string _s, bool oddCharFound);
+
   public:
     bool validPalindrome(std::string &s);
 };
 
+bool Solution::isPalindrome(int left, int right, std::string _s, bool oddCharFound)
+{
+    while (left <= right)
+    {
+        if (_s[left] == _s[right])
+        {
+            left++;
+            right--;
+        }
+        else
+        {
+            if (oddCharFound)
+                return false;
+            oddCharFound = true;
+
+            return isPalindrome(left + 1, right, _s, oddCharFound) or
+                   isPalindrome(left, right - 1, _s, oddCharFound);
+        }
+    }
+
+    return true;
+}
+
 bool Solution::validPalindrome(std::string &s)
 {
-    int oddCharCount = 0;
-    std::map<char, int> charCount;
+    bool oddCharFound = false;
+    int left = 0, right = s.size() - 1;
 
-    for(const char &ch : s)
-        charCount[ch]++;
-
-    for (auto it : charCount)
-        if (it.second % 2)
-            oddCharCount++;
-
-    return (oddCharCount <= 2);
+    return isPalindrome(left, right, s, oddCharFound);
 }
