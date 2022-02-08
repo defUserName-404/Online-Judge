77ff4a6e1bceeaeacf3407d65f6c72938d45bf46
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-02-09 03:06:47 +0600

    Online Judge Solution

--- LeetCode/258_AddDigits.hpp
+++ LeetCode/258_AddDigits.hpp
@@ -1,19 +1,21 @@
 class Solution
 {
   public:
-    int addDigits(int num)
-    {
-        int temp = 0;
-
-        while (num > 0)
-        {
-            temp += (num % 10);
-            num /= 10;
-        }
-
-        if (temp >= 10)
-            return addDigits(temp);
-
-        return temp;
-    }
+    int addDigits(int num);
 };
+
+int Solution::addDigits(int num)
+{
+    int temp = 0;
+
+    while (num > 0)
+    {
+        temp += (num % 10);
+        num /= 10;
+    }
+
+    if (temp >= 10)
+        return addDigits(temp);
+
+    return temp;
+}
