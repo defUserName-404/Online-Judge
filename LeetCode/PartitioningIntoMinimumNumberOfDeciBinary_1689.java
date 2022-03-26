705c7cdb0d29f1e14523c270402dfc678626f5b5
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-03-27 00:11:02 +0600

    Online Judge Solution

---
+++ LeetCode/PartitioningIntoMinimumNumberOfDeciBinary_1689.java
@@ -0,0 +1,14 @@
+public class PartitioningIntoMinimumNumberOfDeciBinary_1689 {
+	public int minPartitions(String number) {
+		int maximumDigit = 0;
+
+		for (int i = 0; i < number.length(); i++) {
+			int currentDigit = number.charAt(i) - '0';
+
+			if (currentDigit > maximumDigit)
+				maximumDigit = currentDigit;
+		}
+
+		return maximumDigit;
+	}
+}
