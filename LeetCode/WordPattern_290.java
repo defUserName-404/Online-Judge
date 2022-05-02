2325f10c402c906cccffb6bc8028320f2776b61b
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-05-03 00:40:55 +0600

    Online Judge Solution

---
+++ LeetCode/WordPattern_290.java
@@ -0,0 +1,27 @@
+import java.util.Map;
+import java.util.HashMap;
+
+class Solution {
+	public boolean wordPattern(String pattern, String s) {
+		String[] words = s.split(" ");
+		Map<Character, String> myMap = new HashMap<>();
+
+		if (pattern.length() != words.length)
+			return false;
+
+		for (int i = 0; i < pattern.length(); i++) {
+			char key = pattern.charAt(i);
+			String word = words[i];
+			boolean doesContainKey = myMap.containsKey(key);
+
+			if (doesContainKey && !myMap.get(key).equals(word))
+				return false;
+			if (!doesContainKey && myMap.containsValue(word))
+				return false;
+
+			myMap.put(key, word);
+		}
+
+		return true;
+	}
+}
