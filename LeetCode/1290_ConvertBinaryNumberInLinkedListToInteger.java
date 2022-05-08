22062f986573f563f7482463fdee01807fa01662
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-05-09 05:14:50 +0600

    Online Judge Solution

---
+++ LeetCode/1290_ConvertBinaryNumberInLinkedListToInteger.java
@@ -0,0 +1,16 @@
+import AlgorithmStudyPlan.ListNode;
+
+class Solution {
+	public int getDecimalValue(ListNode head) {
+		ListNode current = head;
+		StringBuilder value = new StringBuilder();
+
+		while (current != null) {
+			String currentValue = String.valueOf(current.val);
+			value.append(currentValue);
+			current = current.next;
+		}
+
+		return Integer.parseInt(value.toString(), 2);
+	}
+}
