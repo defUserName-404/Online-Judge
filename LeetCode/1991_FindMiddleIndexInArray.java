3014e49c212fb868fc773e9605da3e556e68b48a
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-05-10 05:48:14 +0600

    Online Judge Solution

---
+++ LeetCode/1991_FindMiddleIndexInArray.java
@@ -0,0 +1,24 @@
+class Solution {
+
+	public int findMiddleIndex(int[] nums) {
+		int n = nums.length;
+		int[] prefixSum = new int[n];
+		int sum = 0;
+
+		for (int i = 0; i < n; i++) {
+			sum += nums[i];
+			prefixSum[i] = sum;
+		}
+
+		for (int i = 0; i < n; i++) {
+			int num = nums[i];
+			int left = prefixSum[i] - num;
+			int right = sum - left - num;
+
+			if (left == right)
+				return i;
+		}
+
+		return -1;
+	}
+}
