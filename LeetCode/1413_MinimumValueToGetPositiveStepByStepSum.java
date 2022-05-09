1959412b7ed22367641af0dbe0a53a08c9df529e
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-05-10 05:45:30 +0600

    Online Judge Solution

--- LeetCode/1413_MinimumValueToGetPositiveStepByStepSum.java
+++ LeetCode/1413_MinimumValueToGetPositiveStepByStepSum.java
@@ -1,30 +1,16 @@
 class Solution {
 
 	public int minStartValue(int[] nums) {
-		int n = nums.length;
-		int[] prefixSum = new int[n];
-		int startValue = 1;
-		int min = Integer.MAX_VALUE;
+		int currentSum = 0,
+				min = 0;
 
-		for (int i = 0; i < n; i++) {
-			int num = nums[i];
+		for (final int num : nums) {
+			currentSum += num;
 
-			if (i == 0)
-				prefixSum[i] = num;
-			else
-				prefixSum[i] = prefixSum[i - 1] + num;
-
-			int prefix = prefixSum[i];
-
-			if (prefix < min) {
-				min = prefix;
-
-				if (prefix < 0)
-					startValue = Math.abs(min) + 1;
-			}
-
+			if (currentSum < min)
+				min = currentSum;
 		}
 
-		return startValue;
+		return Math.abs(min) + 1;
 	}
 }
