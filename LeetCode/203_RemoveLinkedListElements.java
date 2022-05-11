af83e0261b4c60472af4221c224cec0ec844949b
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-05-12 04:59:29 +0600

    Online Judge Solution

---
+++ LeetCode/203_RemoveLinkedListElements.java
@@ -0,0 +1,19 @@
+import AlgorithmStudyPlan.ListNode;
+
+class Solution {
+
+	public ListNode removeElements(ListNode head, int val) {
+		ListNode result = new ListNode(0);
+		result.next = head;
+		ListNode current = result;
+
+		while (current.next != null) {
+			if (current.next.val == val)
+				current.next = current.next.next;
+			else
+				current = current.next;
+		}
+
+		return result.next;
+	}
+}
