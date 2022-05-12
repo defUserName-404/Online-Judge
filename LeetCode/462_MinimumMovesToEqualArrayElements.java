2ef42739ce2391604d5ab356806bc198bb19498a
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-05-13 04:53:41 +0600

    Online Judge Solution

---
+++ LeetCode/462_MinimumMovesToEqualArrayElements.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+
+	public int minMoves2(int[] nums) {
+		int n = nums.length;
+		Arrays.sort(nums);
+		int median = nums[n / 2];
+		int result = 0;
+
+		for (final int num : nums)
+			result += Math.abs(median - num);
+
+		return result;
+	}
+}
