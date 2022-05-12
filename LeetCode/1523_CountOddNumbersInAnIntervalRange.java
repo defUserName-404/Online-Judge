bc414e58b786c7d831d47fe94e55df8fc00fc77f
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-05-12 09:16:46 +0600

    Online Judge Solution

---
+++ LeetCode/1523_CountOddNumbersInAnIntervalRange.java
@@ -0,0 +1,13 @@
+class Solution {
+
+	public int countOdds(int low, int high) {
+		int odd = 0;
+
+		if (low % 2 == 1)
+			odd++;
+		if (high % 2 == 1)
+			odd++;
+
+		return (high - low + odd) / 2;
+	}
+}
