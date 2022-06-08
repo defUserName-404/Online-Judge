81d61e83539742545f5f72743ea6d783c43ba895
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-06-08 14:48:19 +0600

    Online Judge Solution

---
+++ LeetCode/1332_RemovePalindromicSubsequences.java
@@ -0,0 +1,11 @@
+class Solution {
+
+	public int removePalindromeSub(String string) {
+		String reverse = new StringBuilder(string).reverse().toString();
+
+		if (string.equals(reverse))
+			return 1;
+
+		return 2;
+	}
+}
