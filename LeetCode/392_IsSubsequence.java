c153393e4ffd0c4dfdfa70d68ce587a1030ed587
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-06-13 21:24:36 +0600

    Online Judge Solution

---
+++ LeetCode/392_IsSubsequence.java
@@ -0,0 +1,24 @@
+class Solution {
+
+	public boolean isSubsequence(String string, String targetString) {
+		int lenOfString = string.length();
+		int lenOfTargetString = targetString.length();
+
+		if ((lenOfString > lenOfTargetString) ||
+				(lenOfString == lenOfTargetString && !string.equals(targetString)))
+			return false;
+
+		int nextCharPosition = 0;
+
+		for (int i = 0; i < lenOfString; i++) {
+			int currentCharPosition = targetString.indexOf(string.charAt(i), nextCharPosition);
+
+			if (currentCharPosition == -1)
+				return false;
+
+			nextCharPosition = currentCharPosition + 1;
+		}
+
+		return true;
+	}
+}
