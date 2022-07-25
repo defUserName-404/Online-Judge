7a10274c7d5a70017157c45371ce39b496364c80
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-07-25 17:04:40 +0600

    Online Judge Solution

---
+++ LeetCode/1464_MaximumProductOfTwoElementsInArray.java
@@ -0,0 +1,20 @@
+class Solution {
+
+	public int maxProduct(int[] nums) {
+		int maxValue, secondMaxValue;
+		maxValue = secondMaxValue = Integer.MIN_VALUE;
+
+		for (final int num : nums) {
+			if (num > maxValue) {
+				secondMaxValue = maxValue;
+				maxValue = num;
+			}
+
+			else if (num > secondMaxValue) {
+				secondMaxValue = num;
+			}
+		}
+
+		return (maxValue - 1) * (secondMaxValue - 1);
+	}
+}
