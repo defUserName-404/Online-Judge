76e5265a9d05f3df7882be8ae9a7f857ea6d351e
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-08-12 17:03:58 +0600

    Online Judge Solution

---
+++ LeetCode/2194_CellsInRangeOnExcelSheet.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+
+	public List<String> cellsInRange(String s) {
+		List<String> sList = new ArrayList<>();
+		char row1 = s.charAt(1),
+				row2 = s.charAt(4),
+				colum1 = s.charAt(0),
+				colum2 = s.charAt(3);
+
+		for (char i = colum1; i <= colum2; i++) {
+			for (char j = row1; j <= row2; j++) {
+				String cell = String.valueOf(i) + String.valueOf(j);
+
+				sList.add(cell);
+			}
+		}
+
+		return sList;
+	}
+}
