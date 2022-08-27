e30de9e484b17ee45852bc546b2c72f8c19d5d3c
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-08-27 09:04:05 +0600

    Online Judge Solution

---
+++ LeetCode/238_ProductOfArrayExceptSelf.java
@@ -0,0 +1,27 @@
+class Solution {
+
+	public int[] productExceptSelf(int[] nums) {
+		int mul = 1;
+		int numberOfZeros = 0;
+		int[] ans = new int[nums.length];
+
+		for (final int num : nums) {
+			if (num == 0) {
+				numberOfZeros++;
+				continue;
+			}
+
+			mul *= num;
+		}
+
+		if (numberOfZeros < 1)
+			for (int i = 0; i < nums.length; i++)
+				ans[i] = mul / nums[i];
+
+		else if (numberOfZeros == 1)
+			for (int i = 0; i < nums.length; i++)
+				ans[i] = nums[i] == 0 ? mul : 0;
+
+		return ans;
+	}
+}
