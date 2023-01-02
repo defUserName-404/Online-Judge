77ae71176b7f70147d8ec1daef37df1740a788b5
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2023-01-02 14:35:10 +0600

    Online Judge Solution

--- ProjectEuler/Test.java
+++ ProjectEuler/Test.java
@@ -1,9 +1,8 @@
 public class Test {
 
 	public static void main(String[] args) {
-		LargeSum_13 solution = new LargeSum_13();
-		String ans = solution.getFirstTenDigitsOfTheSum();
-		System.out.println(ans);
+		SpecialPythagorianTriplet_9 solution = new SpecialPythagorianTriplet_9();
+		System.out.println(solution.getPythagorianTriplet(1000));
 	}
 
 }
