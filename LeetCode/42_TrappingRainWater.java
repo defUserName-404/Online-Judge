8c930939d985363a6d86c34eb94b19f81d083a72
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2023-01-06 11:02:34 +0600

    Online Judge Solution

---
+++ LeetCode/42_TrappingRainWater.java
@@ -0,0 +1,29 @@
+class Solution {
+
+	public int trap(int[] heights) {
+		int left = 0;
+		int right = heights.length - 1;
+		int maxLeft = 0;
+		int maxRight = 0;
+		int waterVolume = 0;
+
+		while (left <= right) {
+			if (heights[left] <= heights[right]) {
+				if (heights[left] > maxLeft)
+					maxLeft = heights[left];
+				else
+					waterVolume += maxLeft - heights[left];
+				left++;
+			} else {
+				if (heights[right] > maxRight)
+					maxRight = heights[right];
+				else
+					waterVolume += maxRight - heights[right];
+				right--;
+			}
+		}
+
+		return waterVolume;
+	}
+
+}
