a212f18744efc045c5f47393a5e5f462cba399ac
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2023-01-13 05:30:54 +0600

    Online Judge Solution

---
+++ LeetCode/2496_MaxValueOfStringInArray.java
@@ -0,0 +1,25 @@
+class Solution {
+
+	public int maximumValue(String[] strs) {
+		int global = 0;
+
+		for (String str : strs) {
+			boolean onlyDigits = (str.charAt(0) >= '0') && (str.charAt(0) <= '9');
+
+			for (char ch : str.toCharArray()) {
+				if (!onlyDigits)
+					break;
+
+				if (!(ch >= '0' && ch <= '9'))
+					onlyDigits = false;
+			}
+
+			int current = onlyDigits ? Integer.parseInt(str) : str.length();
+
+			if (current > global)
+				global = current;
+		}
+
+		return global;
+	}
+}
