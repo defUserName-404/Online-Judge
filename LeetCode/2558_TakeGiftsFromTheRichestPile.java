365f47b6ce7c79b518857cc601ef2e8b3dbf67b8
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2023-02-10 11:39:10 +0600

    Online Judge Solution

---
+++ LeetCode/2558_TakeGiftsFromTheRichestPile.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+
+	public long pickGifts(int[] gifts, int k) {
+		long result = 0;
+
+		while (k != 0) {
+			Arrays.sort(gifts);
+			int currentMax = (int) Math.floor(Math.sqrt(gifts[gifts.length - 1]));
+			gifts[gifts.length - 1] = currentMax;
+			k--;
+		}
+
+		for (int gift : gifts)
+			result += gift;
+
+		return result;
+	}
+
+}
