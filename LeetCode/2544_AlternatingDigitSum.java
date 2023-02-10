cb7ccb27ace354822433d07b19eebe5fc29d4e16
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2023-02-10 11:55:05 +0600

    Online Judge Solution

---
+++ LeetCode/2544_AlternatingDigitSum.java
@@ -0,0 +1,16 @@
+class Solution {
+
+	public int alternateDigitSum(int n) {
+		int sum = 0;
+		boolean isPositive = (String.valueOf(n).length() % 2 == 1);
+
+		while (n > 0) {
+			int digit = n % 10;
+			n /= 10;
+			sum += isPositive ? digit : -digit;
+			isPositive = !isPositive;
+		}
+
+		return sum;
+	}
+}
