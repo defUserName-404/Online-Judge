078012c31ddfd24ea2e34ba81cb06f4a7a61dd9e
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2023-02-11 01:23:38 +0600

    Online Judge Solution

---
+++ LeetCode/2336_SmallestNumberInInfiniteSet.java
@@ -0,0 +1,35 @@
+class SmallestInfiniteSet {
+
+	private byte[] numbers;
+
+	public SmallestInfiniteSet() {
+		numbers = new byte[1001];
+		for (int i = 1; i <= 1000; i++)
+			numbers[i] = 1;
+	}
+
+	public int popSmallest() {
+		int currentSmallest = 0;
+
+		for (int i = 1; i <= 1000; i++) {
+			if (numbers[i] != 0) {
+				currentSmallest = i;
+				numbers[i] = 0;
+				break;
+			}
+		}
+
+		return currentSmallest;
+	}
+
+	public void addBack(int num) {
+		numbers[num] = 1;
+	}
+}
+
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * SmallestInfiniteSet obj = new SmallestInfiniteSet();
+ * int param_1 = obj.popSmallest();
+ * obj.addBack(num);
+ */
