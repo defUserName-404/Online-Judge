939add2cd1a192574e910a10104b09a6851ba984
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2023-02-28 01:08:30 +0600

    Online Judge Solution

---
+++ LeetCode/2562_FindTheArrayConcatanationValue.java
@@ -0,0 +1,21 @@
+class Solution {
+
+	public long findTheArrayConcVal(int[] nums) {
+		long result = 0;
+		int n = nums.length;
+
+		for (int i = 0; i < (n + 1) / 2; i++) {
+			int first = nums[i];
+			int last = nums[nums.length - i - 1];
+
+			if ((i == n / 2) && (n % 2 == 1)) {
+				result += Long.parseLong(String.valueOf(first));
+				break;
+			}
+
+			result += Long.parseLong(String.valueOf(first) + String.valueOf(last));
+		}
+
+		return result;
+	}
+}
