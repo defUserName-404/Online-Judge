2cff3b937033c6813c6ca25e80679cfa371156f0
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2023-03-22 11:12:03 +0600

    Online Judge Solution

---
+++ LeetCode/114_PrintInOrder.java
@@ -0,0 +1,28 @@
+import java.util.concurrent.CountDownLatch;
+
+class Foo {
+
+	private final CountDownLatch firstLatch, secondLatch;
+
+	public Foo() {
+		firstLatch = new CountDownLatch(1);
+		secondLatch = new CountDownLatch(1);
+	}
+
+	public void first(Runnable printFirst) throws InterruptedException {
+		printFirst.run();
+		firstLatch.countDown();
+	}
+
+	public void second(Runnable printSecond) throws InterruptedException {
+		firstLatch.await();
+		printSecond.run();
+		secondLatch.countDown();
+	}
+
+	public void third(Runnable printThird) throws InterruptedException {
+		secondLatch.await();
+		printThird.run();
+	}
+
+}
