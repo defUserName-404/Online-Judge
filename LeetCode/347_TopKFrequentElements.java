ca131cf01ac6c6860dce95266cd707ee901dd328
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2023-03-22 14:45:11 +0600

    Online Judge Solution

--- LeetCode/347_TopKFrequentElements.java
+++ LeetCode/347_TopKFrequentElements.java
@@ -1,4 +1,3 @@
-import java.sql.Array;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -8,13 +7,12 @@ class Solution {
 
 	public int[] topKFrequent(int[] nums, int k) {
 		Map<Integer, Integer> count = new HashMap<>();
-		int[] ans = new int[k];
 		Arrays.stream(nums)
 				.forEach(num -> count.put(num, count.getOrDefault(num, 0) + 1));
 		PriorityQueue<Integer> priorityQueue = new PriorityQueue<>((a, b) -> count.get(b) - count.get(a));
 		count.keySet()
 				.forEach(key -> priorityQueue.add(key));
-
+		int[] ans = new int[k];
 		for (int i = 0; i < k; i++)
 			ans[i] = priorityQueue.poll();
 
