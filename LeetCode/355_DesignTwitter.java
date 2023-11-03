4089c782e6d3d393f9d383d0f97e538dbb6a82b1
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2023-11-03 14:54:33 +0600

    Online Judge solutions

--- LeetCode/355_DesignTwitter.java
+++ LeetCode/355_DesignTwitter.java
@@ -1,26 +1,59 @@
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 class Twitter {
 
-	public Twitter() {
+	private Map<Integer, List<Integer>> followersOfUser;
+	private Map<Integer, List<Tweet>> tweetsOnNewsfeedOfUser;
 
+	public Twitter() {
+		followersOfUser = new HashMap<>();
+		tweetsOnNewsfeedOfUser = new TreeMap<>();
 	}
 
 	public void postTweet(int userId, int tweetId) {
-
+		Tweet tweet = new Tweet(userId, tweetId, Instant.now());
+		tweetsOnNewsfeedOfUser.computeIfAbsent(userId, k -> new ArrayList<>());
+		tweetsOnNewsfeedOfUser.get(userId).add(tweet);
+		List<Integer> followers = followersOfUser.get(userId);
+		if (followers != null) {
+			for (int followerId : followers) {
+				tweetsOnNewsfeedOfUser.computeIfAbsent(followerId, k -> new ArrayList<>());
+				tweetsOnNewsfeedOfUser.get(followerId).add(tweet);
+			}
+		}
 	}
 
 	public List<Integer> getNewsFeed(int userId) {
-		return null;
+		List<Integer> tweetsOnNewsfeed = new ArrayList<>();
+		int count = 0;
+		for (Tweet tweet : tweetsOnNewsfeedOfUser.get(userId)) {
+			tweetsOnNewsfeed.add(tweet.getTweetId());
+			count++;
+			if (count == 10)
+				break;
+		}
+
+		return tweetsOnNewsfeed;
 	}
 
 	public void follow(int followerId, int followeeId) {
-
+		followersOfUser.computeIfAbsent(followerId, k -> new ArrayList<>());
+		followersOfUser.get(followerId).add(followeeId);
 	}
 
 	public void unfollow(int followerId, int followeeId) {
-
+		List<Integer> followers = followersOfUser.get(followerId);
+		if (followers != null) {
+			followers.removeIf(id -> id == followeeId);
+		}
 	}
+
 }
 
 class User {
@@ -34,22 +67,50 @@ class User {
 	public int getUserId() {
 		return userId;
 	}
+
 }
 
-class Tweet {
+class Tweet implements Comparable<Tweet> {
 
 	private final int tweetId;
+	private final Instant timestamp;
 
-	public Tweet(int tweetId) {
+	public Tweet(int userId, int tweetId, Instant timestamp) {
 		this.tweetId = tweetId;
+		this.timestamp = timestamp;
 	}
 
 	public int getTweetId() {
 		return tweetId;
 	}
+
+	public Instant getTimestamp() {
+		return timestamp;
+	}
+
+	@Override
+	public int compareTo(Tweet other) {
+		return other.timestamp.compareTo(this.timestamp);
+	}
+
 }
 
-class TwitterAction {
+class TwitterActions {
 
+	public Map<Integer, List<Integer>> follow(int followerId, int followeeId,
+			Map<Integer, List<Integer>> followersOfUser) {
+		followersOfUser.computeIfAbsent(followerId, k -> new ArrayList<>());
+		followersOfUser.get(followerId).add(followeeId);
+		return followersOfUser;
+	}
 
 }
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter obj = new Twitter();
+ * obj.postTweet(userId,tweetId);
+ * List<Integer> param_2 = obj.getNewsFeed(userId);
+ * obj.follow(followerId,followeeId);
+ * obj.unfollow(followerId,followeeId);
+ */
