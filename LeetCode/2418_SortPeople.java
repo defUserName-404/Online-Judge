bca10425e83c867cf6036c1c5fbb2428b7e8ecac
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2023-11-10 23:08:56 +0600

    Online Judge solutions

--- LeetCode/2418_SortPeople.java
+++ LeetCode/2418_SortPeople.java
@@ -6,14 +6,17 @@ class Solution {
 	public String[] sortPeople(String[] names, int[] heights) {
 		int n = names.length;
 		Integer[] heightsSorted = new Integer[n];
-		for (int i = 0; i < n; i++)
+		for (int i = 0; i < n; i++) {
 			heightsSorted[i] = heights[i];
+		}
 		Arrays.sort(heightsSorted, Collections.reverseOrder());
 		String[] namesSorted = new String[n];
 
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				if (heightsSorted[i] == heights[j]) namesSorted[i] = names[j];
+				if (heightsSorted[i] == heights[j]) {
+					namesSorted[i] = names[j];
+				}
 			}
 		}
 
