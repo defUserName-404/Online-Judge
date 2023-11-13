39bf950a81cf96a339413dacff99ce7620dee671
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2023-11-13 23:25:15 +0600

    Online Judge solutions

--- LeetCode/242_ValidAnagram.java
+++ LeetCode/242_ValidAnagram.java
@@ -2,16 +2,16 @@ import java.util.stream.Collectors;
 
 class Solution {
 
-	private String sortString(String string) {
-		return string.chars()
-				.sorted()
-				.mapToObj(c -> String.valueOf(c))
-				.collect(Collectors.joining());
-	}
+  private String sortString(String string) {
+    return string.chars()
+        .sorted()
+        .mapToObj(String::valueOf)
+        .collect(Collectors.joining());
+  }
 
-	public boolean isAnagram(String string1, String string2) {
-		return sortString(string1)
-				.equals(sortString(string2));
-	}
+  public boolean isAnagram(String string1, String string2) {
+    return sortString(string1)
+        .equals(sortString(string2));
+  }
 
 }
