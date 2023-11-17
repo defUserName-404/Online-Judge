306abe268497e678479fb96abb2d0d0e4d623f86
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2023-11-17 17:46:46 +0600

    Online Judge solutions

--- LeetCode/Pandas/solutions/gameplay_analysis_i_511.py
+++ LeetCode/Pandas/solutions/gameplay_analysis_i_511.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def game_analysis(activity: pd.DataFrame) -> pd.DataFrame:
     return (
-        activity.loc[pd.DataFrame(activity).groupby("player_id")["event_date"].idxmin()]
-        .drop(["device_id", "games_played"], axis=1)
-        .rename(columns={"player_id": "player_id", "event_date": "first_login"})
-    )
+        activity.loc[
+            pd.DataFrame(activity).groupby("player_id")["event_date"].idxmin()
+        ].rename(columns={"player_id": "player_id", "event_date": "first_login"})
+    )[["player_id", "first_login"]]
