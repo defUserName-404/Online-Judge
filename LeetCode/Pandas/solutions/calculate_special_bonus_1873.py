0bd7265cfd3ba629c4e8c6c8fd4959747dd3205c
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2023-11-27 05:19:22 +0600

    Online Judge solutions

---
+++ LeetCode/Pandas/solutions/calculate_special_bonus_1873.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+def is_applicable_for_bonus(employee: pd.Series) -> bool:
+    return (employee["employee_id"] % 2 != 0) and (
+        not str(employee["name"]).startswith("M")
+    )
+
+
+def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
+    employees["bonus"] = (
+        employees.apply(is_applicable_for_bonus, axis=1).astype(int)
+        * employees["salary"]
+    )
+    return employees[["employee_id", "bonus"]].sort_values(by="employee_id")
