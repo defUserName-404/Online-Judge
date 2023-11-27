eb32e50746e98ea07fa9b06866ca9d8e242e9184
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2023-11-28 05:18:40 +0600

    Online Judge solutions

---
+++ LeetCode/Pandas/solutions/second_highest_salary_176.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
+    unique_sorted_salaries = sorted(employee["salary"].unique())
+    return pd.DataFrame(
+        {
+            "SecondHighestSalary": [
+                unique_sorted_salaries[-2] if len(unique_sorted_salaries) > 1 else None
+            ]
+        }
+    )
