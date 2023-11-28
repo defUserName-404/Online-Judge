7be8ff6b359c95bb1231769b96c9a53678c5603b
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2023-11-28 09:23:36 +0600

    Online Judge solutions

--- LeetCode/Pandas/main.py
+++ LeetCode/Pandas/main.py
@@ -2,14 +2,14 @@ import pandas as pd
 
 
 def test():
-    data = {
-        "name": ["Tatiana", "Khaled", "Alex", "Jonathan", "Stefan", "Tommy"],
-        "species": ["Snake", "Giraffe", "Leopard", "Monkey", "Bear", "Panda"],
-        "age": [98, 50, 6, 45, 100, 26],
-        "weight": [379, 410, 328, 50, 912, 347],
-    }
-    animals = pd.DataFrame(data)
-    print(animals.query("weight > 100").sort_values("weight", ascending=False)["name"])
+    data = [[1, 2, 3], [1, 2, 4], [1, 3, 3], [2, 1, 1], [2, 2, 1], [2, 3, 1], [2, 4, 1]]
+    teacher = pd.DataFrame(
+        data, columns=["teacher_id", "subject_id", "dept_id"]
+    ).astype({"teacher_id": "Int64", "subject_id": "Int64", "dept_id": "Int64"})
+    teacher = (
+        teacher.groupby("teacher_id")["subject_id"].nunique().reset_index(name="cnt")
+    )
+    print(teacher)
 
 
 if __name__ == "__main__":
