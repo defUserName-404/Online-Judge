9bcedc119def4270f08711e80fd557c0ec729f89
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2023-12-07 17:04:47 +0600

    Online Judge solutions

--- LeetCode/Pandas/solutions/second_highest_salary_176.py
+++ LeetCode/Pandas/solutions/second_highest_salary_176.py
@@ -4,9 +4,9 @@ import pandas as pd
 def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
     unique_sorted_salaries = sorted(employee["salary"].unique())
     return pd.DataFrame(
-            {
-                    "SecondHighestSalary": [
-                            unique_sorted_salaries[-2] if len(unique_sorted_salaries) > 1 else None
-                    ]
-            }
+        {
+            "SecondHighestSalary": [
+                unique_sorted_salaries[-2] if len(unique_sorted_salaries) > 1 else None
+            ]
+        }
     )
