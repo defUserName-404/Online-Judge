71884c6feed324bfa5c86904fb661802376532a7
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2024-01-02 22:54:29 +0600

    Online Judge solutions

---
+++ LeetCode/3014_MinimumNumberOfPushesToTypeWordI.java
@@ -0,0 +1,17 @@
+class Solution {
+
+	public int minimumPushes(String word) {
+		int len = word.length();
+		int pushes = 0;
+		int i = 1;
+
+		while (len > 0) {
+			pushes += (Math.min(len, 8) * i);
+			len -= 8;
+			i++;
+		}
+
+		return pushes;
+	}
+
+}
