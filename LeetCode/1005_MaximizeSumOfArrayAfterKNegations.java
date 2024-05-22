da04ac8cc3f9120b4150e4179092737e5edd4727
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2024-05-23 00:21:44 +0600

    Online judge solutions

--- LeetCode/1005_MaximizeSumOfArrayAfterKNegations.java
+++ LeetCode/1005_MaximizeSumOfArrayAfterKNegations.java
@@ -1,17 +1,40 @@
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 class Solution {
-	
-	public int largestSumAfterKNegations(int[] nums, int k) {
-		Arrays.sort(nums);
-		for (int i = 0; k > 0 && i < nums.length && nums[i] < 0; i++, k--) {
-			nums[i] = -nums[i];
 
+	/*
+	 * Solution 1
+	 */
+//	public int largestSumAfterKNegations(int[] nums, int k) {
+//		Arrays.sort(nums);
+//		for (int i = 0; k > 0 && i < nums.length && nums[i] < 0; i++, k--) {
+//			nums[i] = -nums[i];
+//
+//		}
+//		return Arrays.stream(nums)
+//					 .sum() - (k % 2) * Arrays.stream(nums)
+//											  .min()
+//											  .orElse(0) * 2;
+//	}
+
+	/*
+	 * Solution 2
+	 */
+	public int largestSumAfterKNegations(int[] nums, int k) {
+		PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+		int sum = 0;
+		for (int num : nums) {
+			minHeap.add(num);
+			sum += num;
 		}
-		return Arrays.stream(nums)
-					 .sum() - (k % 2) * Arrays.stream(nums)
-											  .min()
-											  .orElse(0) * 2;
+		while (k != 0) {
+			int num = minHeap.remove();
+			sum -= num * 2;
+			minHeap.add(-num);
+			k--;
+		}
+		return sum;
 	}
 
 }
