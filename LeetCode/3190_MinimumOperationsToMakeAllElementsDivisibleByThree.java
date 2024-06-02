7902af295d8bad127a1881a259358a3fb2f56380
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2024-06-02 12:57:33 +0600

    Online judge solutions

---
+++ LeetCode/3190_MinimumOperationsToMakeAllElementsDivisibleByThree.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Solution {
+
+	public int minimumOperations(int[] nums) {
+		return (int) Arrays.stream(nums)
+						   .filter(num -> num % 3 != 0)
+						   .count();
+	}
+
+}
