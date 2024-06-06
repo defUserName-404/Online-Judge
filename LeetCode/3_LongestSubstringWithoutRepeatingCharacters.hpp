8368dd5f108f4142183e251760b0b105b3531f8e
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2024-06-06 13:17:38 +0600

    Online judge solutions

--- LeetCode/3_LongestSubstringWithoutRepeatingCharacters.hpp
+++ LeetCode/3_LongestSubstringWithoutRepeatingCharacters.hpp
@@ -11,13 +11,11 @@ int Solution::lengthOfLongestSubstring(std::string s)
     int n = s.size();
     int startingPointOfChars[256] = {[0 ... 255] = -1};
     int longest = 0, startingPosition = -1;
-
     for (int i = 0; i < n; i++)
     {
         startingPosition = std::max(startingPointOfChars[s[i]], startingPosition);
         startingPointOfChars[s[i]] = i;
         longest = std::max(longest, i - startingPosition);
     }
-
     return longest;
 }
