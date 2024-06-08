9512b898cc286737b91ac3e65cf6a5ed03808005
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2024-06-08 13:19:13 +0600

    Online judge solutions

--- LeetCode/11_ContainerWithMostWater.java
+++ LeetCode/11_ContainerWithMostWater.java
@@ -3,22 +3,17 @@ class Solution {
 	public int maxArea(int[] height) {
 		int left = 0, right = height.length - 1;
 		int maxWaterArea = 0;
-
 		while (left < right) {
 			int currentWaterArea = Math.min(height[left], height[right]) * (right - left);
-
 			if (currentWaterArea > maxWaterArea) {
 				maxWaterArea = currentWaterArea;
 			}
-
 			if (height[left] < height[right]) {
 				left++;
-			}
-			else {
+			} else {
 				right--;
 			}
 		}
-
 		return maxWaterArea;
 	}
 
