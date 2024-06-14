3b227612314a5102e76e9294b3eadd91b698c743
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2024-06-14 13:22:06 +0600

    Online judge solutions

--- LeetCode/49_GroupAnagrams.java
+++ LeetCode/49_GroupAnagrams.java
@@ -9,24 +9,19 @@ class Solution {
 	private String sortedString(String string) {
 		char[] stringToCharArray = string.toCharArray();
 		Arrays.sort(stringToCharArray);
-
 		return new String(stringToCharArray);
 	}
 
 	public List<List<String>> groupAnagrams(String[] strings) {
 		Map<String, List<String>> stringOccurranceMap = new HashMap<>();
-
 		for (final String string : strings) {
 			String stringSorted = sortedString(string);
-
 			List<String> anagrams = stringOccurranceMap.containsKey(stringSorted)
-				? stringOccurranceMap.get(stringSorted)
-				: new ArrayList<>();
-
+					? stringOccurranceMap.get(stringSorted)
+					: new ArrayList<>();
 			anagrams.add(string);
 			stringOccurranceMap.put(stringSorted, anagrams);
 		}
-
 		return new ArrayList<>(stringOccurranceMap.values());
 	}
 
