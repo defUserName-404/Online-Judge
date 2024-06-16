cd0e955b0a7b945074253367e986e1cb139c033b
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2024-06-16 13:23:23 +0600

    Online judge solutions

--- LeetCode/137_SingleNumberII.java
+++ LeetCode/137_SingleNumberII.java
@@ -2,12 +2,10 @@ class Solution {
 
 	public int singleNumber(int[] nums) {
 		int ones = 0, twos = 0;
-
 		for (final int num : nums) {
 			ones = (ones ^ num) & (~twos);
 			twos = (twos ^ num) & (~ones);
 		}
-
 		return ones;
 	}
 
