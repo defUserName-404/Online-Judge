a43351cd8539b45032daf87553b940cd41ec4990
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2024-06-18 13:24:27 +0600

    Online judge solutions

--- LeetCode/153_FindMinimumInRotatedSortedArray.java
+++ LeetCode/153_FindMinimumInRotatedSortedArray.java
@@ -2,17 +2,14 @@ class Solution {
 
 	public int findMin(int[] nums) {
 		int left = 0, right = nums.length - 1;
-
 		while (left < right) {
 			int mid = (left + right) / 2;
 			if (nums[mid] > nums[right]) {
 				left = mid + 1;
-			}
-			else {
+			} else {
 				right = mid;
 			}
 		}
-
 		return nums[left];
 	}
 
