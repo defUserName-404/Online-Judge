ec2ee4cf45ebaead652d3765950d6fa0cfa2a6e3
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2024-06-26 13:29:07 +0600

    Online judge solutions

--- LeetCode/278_FirstBadVersion.java
+++ LeetCode/278_FirstBadVersion.java
@@ -1,15 +1,10 @@
-import java.util.concurrent.ThreadLocalRandom;
-
-/* The isBadVersion API is defined in the parent class VersionControl.
-      boolean isBadVersion(int version); */
+import java.util.concurrent.ThreadLocalRandom;  /* The isBadVersion API is defined in the parent class VersionControl.       boolean isBadVersion(int version); */
 
 class VersionControl {
 
 	public static boolean isBadVersion(int n) {
-		int randomNum = ThreadLocalRandom
-			.current()
-			.nextInt(1, n + 1);
-
+		int randomNum = ThreadLocalRandom.current()
+										 .nextInt(1, n + 1);
 		return (randomNum == n);
 	}
 
@@ -19,19 +14,15 @@ class Solution {
 
 	public int firstBadVersion(int n) {
 		int left = 1, right = n;
-
 		while (left < right) {
 			int mid = left + (right - left) / 2;
-
 			if (VersionControl.isBadVersion(mid)) {
 				right = mid;
-			}
-			else {
+			} else {
 				left = mid + 1;
 			}
 		}
-
 		return right;
 	}
 
-}
+} 
