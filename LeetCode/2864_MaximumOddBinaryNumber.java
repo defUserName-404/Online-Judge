eab94b448b51bf6eb6c0057613d66abe8ef5c004
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2024-07-11 09:38:53 +0600

    Online judge solutions

--- LeetCode/2864_MaximumOddBinaryNumber.java
+++ LeetCode/2864_MaximumOddBinaryNumber.java
@@ -4,11 +4,10 @@ class Solution {
 		int oneCount = (int) s.chars()
 							  .filter(ch -> ch == '1')
 							  .count();
-		StringBuilder result = new StringBuilder();
-		result.append("1".repeat(oneCount - 1))
-			  .append("0".repeat(s.length() - oneCount))
-			  .append('1');
-		return result.toString();
+		return new StringBuilder().append("1".repeat(oneCount - 1))
+								  .append("0".repeat(s.length() - oneCount))
+								  .append('1')
+								  .toString();
 	}
 
 }
