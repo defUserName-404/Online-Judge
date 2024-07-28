c8cc462bae0fc2bb11c11af7ccef06729c8577fe
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2024-07-28 12:36:50 +0600

    Online judge solution

--- LeetCode/46_Permutations.hpp
+++ LeetCode/46_Permutations.hpp
@@ -14,7 +14,6 @@ void Solution::permuteHelper(std::vector<int> nums, int l, int r)
 {
     if (l == r)
         ans.push_back(nums);
-
     for (int i = l; i <= r; i++)
     {
         std::swap(nums[l], nums[i]);
@@ -26,6 +25,5 @@ void Solution::permuteHelper(std::vector<int> nums, int l, int r)
 std::vector<std::vector<int>> Solution::permute(std::vector<int> &nums)
 {
     permuteHelper(nums, 0, nums.size() - 1);
-
     return ans;
 }
