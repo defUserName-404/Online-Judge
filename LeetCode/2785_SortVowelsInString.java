ab22f616b5a6c42a2d7689e7192eaf0d5604b92e
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2024-08-06 14:40:45 +0600

    Online judge solution

--- LeetCode/2785_SortVowelsInString.java
+++ LeetCode/2785_SortVowelsInString.java
@@ -1,23 +1,22 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.PriorityQueue;
+import java.util.stream.Collectors;
 
 class Solution {
 
 	public String sortVowels(String s) {
-		List<Character> vowels = new ArrayList<>();
-		StringBuilder result = new StringBuilder();
+		var queue = s.chars()
+					 .filter(ch -> isVowel((char) ch))
+					 .mapToObj(c -> (char) c)
+					 .collect(Collectors.toCollection(PriorityQueue::new));
+		var result = new StringBuilder();
 		for (int i = 0; i < s.length(); i++) {
 			char c = s.charAt(i);
 			if (isVowel(c)) {
-				vowels.add(c);
+				char vowel = queue.remove();
+				result.append(vowel);
+				continue;
 			}
-		}
-		Collections.sort(vowels);
-		int vowelIndex = 0;
-		for (int i = 0; i < s.length(); i++) {
-			char c = s.charAt(i);
-			result.append(isVowel(c) ? vowels.get(vowelIndex++) : c);
+			result.append(c);
 		}
 		return result.toString();
 	}
