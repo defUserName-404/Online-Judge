ec7f07b9929fd3fef1effaa7f03cc0fbcc09244a
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2024-08-13 06:00:53 +0600

    Online judge solution

---
+++ LeetCode/2710_RemoveTrailingZerosFromAString.java
@@ -0,0 +1,22 @@
+class Solution {
+
+	/*
+	 * Solution 1: Iterate through the string and remove trailing zeros
+	 */
+
+//	public String removeTrailingZeros(String num) {
+//		int i = num.length() - 1;
+//		while (i >= 0 && num.charAt(i) == '0') {
+//			i--;
+//		}
+//		return num.substring(0, i + 1);
+//	}
+
+	/*
+	 * Solution 2: Use of regex
+	 */
+	public String removeTrailingZeros(String num) {
+		return num.replaceAll("0*$", "");
+	}
+
+}
