6dd2af2c9ccb727bed7c0566d4d827ab39165d1a
NextPage02 <moshiur.r.nafi@gmail.com>
2024-10-20 12:26:13 +0600

    Online judge solution

--- LeetCode/506_RelativeRanks.java
+++ LeetCode/506_RelativeRanks.java
@@ -1,16 +1,25 @@
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 class Solution {
 
-	public String[] findRelativeRanks(Integer[] score) {
-		Map<Integer, Integer> scoreAndPosition = new HashMap<>();
-		String[] rank = new String[score.length];
-		Arrays.sort(score, Collections.reverseOrder());
-
-		return rank;
-	}
+    public String[] findRelativeRanks(int[] score) {
+        int n = score.length;
+        int[][] scoreAndPosition = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            scoreAndPosition[i] = new int[]{i, score[i]};
+        }
+        String[] ranks = new String[n];
+        Arrays.sort(scoreAndPosition, (a, b) -> b[1] - a[1]);
+        for (int i = 0; i < n; i++) {
+            String rank = switch (i) {
+                case 0 -> "Gold Medal";
+                case 1 -> "Silver Medal";
+                case 2 -> "Bronze Medal";
+                default -> String.valueOf(i + 1);
+            };
+            ranks[scoreAndPosition[i][0]] = rank;
+        }
+        return ranks;
+    }
 
 }
