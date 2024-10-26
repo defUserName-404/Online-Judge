22ea652d8f6cb10e6d046e3bb2fb7df144aedaad
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2024-10-26 10:29:12 +0600

    Online judge solution

--- LeetCode/NeetCode150/17_LetterCombinationsOfPhoneNumber.java
+++ LeetCode/NeetCode150/17_LetterCombinationsOfPhoneNumber.java
@@ -4,27 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    private final String[] map = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    private final String[] phoneNumberToStringMap = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
     private final List<String> result = new ArrayList<>();
 
     public List<String> letterCombinations(String digits) {
         if (digits.isEmpty()) {
             return result;
         }
-        backtrack(new StringBuilder(), digits, 0);
+        addCurrentDigit(new StringBuilder(), digits, 0);
         return result;
     }
 
-    private void backtrack(StringBuilder combination, String digits, int index) {
+    private void addCurrentDigit(StringBuilder combination, String digits, int index) {
         if (index == digits.length()) {
             result.add(combination.toString());
             return;
         }
         int digit = digits.charAt(index) - '0';
-        String letters = map[digit];
+        String letters = phoneNumberToStringMap[digit];
         for (char letter : letters.toCharArray()) {
             combination.append(letter);
-            backtrack(combination, digits, index + 1);
+            addCurrentDigit(combination, digits, index + 1);
             combination.deleteCharAt(combination.length() - 1);
         }
     }
