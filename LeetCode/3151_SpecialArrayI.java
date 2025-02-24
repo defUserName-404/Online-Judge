a202103285e11ffb6b550a790c8d0fce7138bca1
defUserName-404 <moshiur.r.nafi@gmail.com>
2025-02-24 12:07:19 +0600

    online judge solution

--- LeetCode/3151_SpecialArrayI.java
+++ LeetCode/3151_SpecialArrayI.java
@@ -1,12 +1,7 @@
 class Solution {
-
-    private boolean isEven(int x) {
-        return x % 2 == 0;
-    }
-
     public boolean isArraySpecial(int[] nums) {
         for (int i = 0; i < nums.length - 1; i++) {
-            if ((isEven(nums[i]) && isEven(nums[i + 1])) || (!isEven(nums[i]) && !isEven(nums[i + 1]))) {
+            if (nums[i] % 2 == nums[i + 1] % 2) {
                 return false;
             }
         }
