31cba4649902285129fc5d63a2da11a11721af12
defUserName-404 <moshiur.r.nafi@gmail.com>
2025-05-24 10:35:42 +0600

    Online judge solution

--- LeetCode/226_InvertBinaryTree.java
+++ LeetCode/226_InvertBinaryTree.java
@@ -1,15 +1,26 @@
-import AlgorithmStudyPlan.TreeNode;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
 
 class Solution {
-
-	public TreeNode invertTree(TreeNode root) {
-		if (root == null) {
-			return null;
-		}
-		TreeNode leftNode = invertTree(root.left);
-		root.left = invertTree(root.right);
-		root.right = leftNode;
-		return root;
-	}
-
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)
+            return null;
+        TreeNode leftNode = invertTree(root.left);
+        root.left = invertTree(root.right);
+        root.right = leftNode;
+        return root;
+    }
 }
