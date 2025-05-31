3de73866e27d7db3d22fb27b76b165f950f50163
defUserName-404 <moshiur.r.nafi@gmail.com>
2025-05-31 13:30:56 +0600

    Online judge solution

--- LeetCode/34_FindFirst&LastPositionOfElementInSortedArray.java
+++ LeetCode/34_FindFirst&LastPositionOfElementInSortedArray.java
@@ -3,14 +3,12 @@ class Solution {
 	private int[] nums;
 	private int target;
 
-	private int getOccurrance(Position position) {
+	private int getOccurrence(Position position) {
 		int low = 0, high = nums.length - 1;
 		int index = -1;
-
 		while (low <= high) {
 			int mid = (low + high) / 2;
 			int value = nums[mid];
-
 			if (value > target) {
 				high = mid - 1;
 			}
@@ -27,22 +25,18 @@ class Solution {
 				}
 			}
 		}
-
 		return index;
 	}
 
 	public int[] searchRange(int[] nums, int target) {
 		this.nums = nums;
 		this.target = target;
-
-		int firstIndex = getOccurrance(Position.FIRST);
-		int lastIndex = getOccurrance(Position.LAST);
-
+		int firstIndex = getOccurrence(Position.FIRST);
+		int lastIndex = getOccurrence(Position.LAST);
 		return new int[]{firstIndex, lastIndex};
 	}
 
-	private static enum Position {
-
+	private enum Position {
 		FIRST,
 		LAST
 	}
