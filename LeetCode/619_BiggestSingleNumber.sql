efc231d4cded8c3c857ac8621523ffa6dbd31036
defUserName-404 <moshiur.r.nafi@gmail.com>
2025-09-04 15:53:59 +0600

    Online judge solution

---
+++ LeetCode/619_BiggestSingleNumber.sql
@@ -0,0 +1,70 @@
+-- SQL SCHEMA
+-- Create table If Not Exists MyNumbers
+-- (
+--     num
+--     int
+-- )
+--     Truncate table MyNumbers
+--     insert into MyNumbers
+-- (
+--     num
+-- ) values
+-- (
+--     '8'
+-- )
+--     insert into MyNumbers
+-- (
+--     num
+-- ) values
+-- (
+--     '8'
+-- )
+--     insert into MyNumbers
+-- (
+--     num
+-- ) values
+-- (
+--     '3'
+-- )
+--     insert into MyNumbers
+-- (
+--     num
+-- ) values
+-- (
+--     '3'
+-- )
+--     insert into MyNumbers
+-- (
+--     num
+-- ) values
+-- (
+--     '1'
+-- )
+--     insert into MyNumbers
+-- (
+--     num
+-- ) values
+-- (
+--     '4'
+-- )
+--     insert into MyNumbers
+-- (
+--     num
+-- ) values
+-- (
+--     '5'
+-- )
+--     insert into MyNumbers
+-- (
+--     num
+-- ) values
+-- (
+--     '6'
+-- )
+
+SELECT MAX(num) AS num
+FROM MyNumbers
+WHERE num IN (SELECT num
+              FROM MyNumbers
+              GROUP BY num
+              HAVING COUNT(num) = 1);
