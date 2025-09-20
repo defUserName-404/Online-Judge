e602ed0b12de0c4f679c69e2dfef0de5943663a1
defUserName-404 <moshiur.r.nafi@gmail.com>
2025-09-20 14:06:23 +0600

    Online solution solution

---
+++ FreeCodeCamp/DailyCodingChallenge/ArrayDiff.js
@@ -0,0 +1,8 @@
+// ! 2025-09-10
+//
+// ? Given two arrays with strings values, return a new array containing all the values that appear in only one of the arrays.
+// * - The returned array should be sorted in alphabetical order.
+function arrayDiff(arr1, arr2) {
+
+    return arr1;
+}
