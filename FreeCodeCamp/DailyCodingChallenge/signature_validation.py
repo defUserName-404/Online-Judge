90e72884b1534d66c4ed85ae5dd1c8ec1e483915
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2025-11-09 09:47:17 +0600

    Online judge solution

---
+++ FreeCodeCamp/DailyCodingChallenge/signature_validation.py
@@ -0,0 +1,27 @@
+# ! 2025-11-01
+# ? Given a message string, a secret key string, and a signature number, determine if the signature is valid using this encoding method:
+# ? - Letters in the message and secret key have these values:
+# ? - a to z have values 1 to 26 respectively.
+# ? - A to Z have values 27 to 52 respectively.
+# ? - All other characters have no value.
+# ? - Compute the signature by taking the sum of the message plus the sum of the secret key.
+# ? For example, given the message "foo" and the secret key "bar", the signature would be 57:
+# ? f (6) + o (15) + o (15) = 36
+# ? b (2) + a (1) + r (18) = 21
+# ? 36 + 21 = 57
+# ? Check if the computed signature matches the provided signature.
+
+def verify(message, key, signature):
+    message_sum = __decode_value(message)
+    key_sum = __decode_value(key)
+    return message_sum + key_sum == signature
+
+
+def __decode_value(string):
+    value = 0
+    for char in string:
+        if "a" <= char <= "z":
+            value += ord(char) - ord("a") + 1
+        elif "A" <= char <= "Z":
+            value += ord(char) - ord("A") + 27
+    return value
