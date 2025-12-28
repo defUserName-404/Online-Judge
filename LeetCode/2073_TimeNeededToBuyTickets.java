ed3b03600a7c7550878ea1629faa15b87009c3c5
defUserName-404 <moshiur.r.nafi@gmail.com>
2025-12-28 13:18:56 +0600

    Online judge solution

--- LeetCode/2073_TimeNeededToBuyTickets.java
+++ LeetCode/2073_TimeNeededToBuyTickets.java
@@ -1,9 +1,17 @@
+import java.util.stream.IntStream;
+
 class Solution {
+//    public int timeRequiredToBuy(int[] tickets, int k) {
+//        int total = 0;
+//        for (int i = 0; i < tickets.length; i++) {
+//            total += (i <= k) ? Math.min(tickets[i], tickets[k]) : Math.min(tickets[i], tickets[k] - 1);
+//        }
+//        return total;
+//    }
+
     public int timeRequiredToBuy(int[] tickets, int k) {
-        int total = 0;
-        for (int i = 0; i < tickets.length; i++) {
-            total += (i <= k) ? Math.min(tickets[i], tickets[k]) : Math.min(tickets[i], tickets[k] - 1);
-        }
-        return total;
+        return IntStream.range(0, tickets.length)
+                .map(i -> Math.min(tickets[i], tickets[k] - (i > k ? 1 : 0)))
+                .sum();
     }
 }
