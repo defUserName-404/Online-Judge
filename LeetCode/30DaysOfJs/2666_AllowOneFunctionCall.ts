a443091c837ccb95207bf0eb8e7465913b6848de
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2026-03-19 19:10:45 +0600

    Online judge solution

---
+++ LeetCode/30DaysOfJs/2666_AllowOneFunctionCall.ts
@@ -0,0 +1,18 @@
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined
+
+function once(fn: Function): OnceFn {
+  let hasBeenCalled = false;
+  return (...args) => {
+    if (hasBeenCalled) return undefined;
+    hasBeenCalled = true;
+    return fn(...args);
+  }
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
